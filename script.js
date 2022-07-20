bd7e85735d2d4813f7ac71d48632a612975439df
Ieva <106997549+ikontrimaite@users.noreply.github.com>
2022-07-20 23:46:01 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+"use strict"
+
+// PIRMA UŽDUOTIS
+// Parašykite scriptą, kuris paprašytų įvesti: prekės kainą, paklaustų ar reikalingas pristatymas, jei reikalingas - paklaustų miesto į kurį reiks pristatyti. Naudokit do while loop, kol kažką įrašys arba įrašys teisingai ir if bei else kad patikrintų ką įrašys.
+
+// "Įveskite prekės kainą";
+// "Ar reikalingas pristatymas į namus? (taip/ne)";
+// "Į kurį miestą reiks pristatyti?"
+
+// Galiausiai atspausdintų tokią informaciją:
+
+// ---
+// 1 variantas (be pristatymo)
+
+// Prekės kaina: xx.xx €
+// Prekę galite atsiimti nemokamai Vilniuje adresu Gedimino pr. 1a
+
+
+// ---
+// 2 variantas (su pristatymu)
+
+// Prekės kaina : xx.xx €
+// Pristatymas: xx.xx €
+// Iš viso: xx.xx €
+// Prekę pristatysime į "įvestas miestas" per 1-3 dienas.
+
+
+var kaina = prompt("Įveskite prekės kainą")
+kaina = Number (kaina)
+
+while(isNaN(kaina)){
+    var kaina = prompt("Įveskite prekės kainą")
+    kaina = Number (kaina)
+}
+console.log ("Prekės kaina: " + kaina + "€")
